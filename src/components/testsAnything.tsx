f532b7d8acef14814a2ee8969fd8bdeedd40ed3f
kevenjq <kevenjq07@gmail.com>
2025-05-19 15:43:31 +0200

    fix lenis components

--- src/components/testsAnything.tsx
+++ src/components/testsAnything.tsx
@@ -1,18 +1,9 @@
-import { ReactLenis, useLenis } from "lenis/react";
-
 function App() {
-  const lenis = useLenis(({ scroll }) => {
-    // called every scroll
-  });
-
   return (
-    <ReactLenis root>
-      {
-        <div>
-          <h1>testing lenis</h1>
-        </div>
-      }
-    </ReactLenis>
+    <div>
+      <h1>Test Anything</h1>
+      <p>This is a test for the Anything component.</p>
+    </div>
   );
 }
 
