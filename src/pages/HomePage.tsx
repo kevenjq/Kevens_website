85f24f454c1b62cb488fd7fc26213a3c2f7e4f93
kevenjq <kevenjq07@gmail.com>
2026-01-17 11:56:21 +0100

    progress check : homepage is 50% done not including animation tho so..

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,17 @@
 import Navbar from "../components/NavBar/Navbar";
 import styled from "styled-components";
 import ContactList from "../components/ContactList/ContactList";
+import { useEffect } from "react";
 
 const PageContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
-
-const IntroCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-self: center;
-  justify-content: center;
-  width: 80rem;
-  max-width: 95vw;
-  padding: 5px 10px 5px 10px;
-  gap: 5rem;
-  margin-bottom: 0rem;
-`;
-
-const AboutCard = styled(IntroCard)`
-  justify-content: start;
-`;
-
 const PageCards = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 100px;
+
+  margin-top: 7.5rem;
   width: 100dvw;
   height: calc(100vh - 105px);
 
@@ -39,6 +24,33 @@ const PageCards = styled.div`
     scroll-snap-stop: always;
     aspect-ratio: 2/1;
   }
+`;
+
+const IntroCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  justify-content: start;
+  width: 80rem;
+  padding: 0 2rem 1rem 2rem;
+  gap: 5rem;
+  margin-bottom: 2rem;
+`;
+
+const AboutCard = styled(IntroCard)`
+  padding: 1rem 2rem 1rem 2rem;
+`;
+
+const ExperienceCard = styled(IntroCard)``;
+
+const BlogUpComing = styled(IntroCard)``;
+
+const FooterCard = styled(IntroCard)`
+  > div > h1 {
+    font-size: 2rem;
+  }
+  flex-direction: row;
+  padding: 5rem;
 `;
 
 const TextChanger = styled.div`
@@ -62,22 +74,22 @@ const ItemText = styled.h1`
   display: block;
   height: 100%;
   color: var(--text-changer);
-  animation: TextChanger 10s infinite;
+  animation: TextChanger 11s infinite;
 
   @keyframes TextChanger {
     10% {
       transform: translateY(-100%);
     }
     
-    25% {
+    20% {
       transform: translateY(-200%);
     }
 
-    50% {
+    60% {
       transform: translateY(-300%);
     }
 
-    75% {
+    80% {
       transform: translateY(-400%);
     }
     
@@ -88,11 +100,29 @@ const ItemText = styled.h1`
 `;
 
 const HomePage = () => {
+  // ::selection get rid of it if user lets go.
+  useEffect(() => {
+    const clear = () => window.getSelection()?.removeAllRanges();
+
+    document.addEventListener("mouseup", clear);
+    document.addEventListener("touchend", clear);
+
+    return () => {
+      document.removeEventListener("mouseup", clear);
+      document.removeEventListener("touchend", clear);
+    };
+  }, []);
   return (
     <PageContainer>
+      {/*Nav Bar Component*/}
       <Navbar />
+
+      {/*Contact List Component*/}
       <ContactList />
+
+      {/*Homepage each Card are sections*/}
       <PageCards>
+        {/*First Card - Welcome page*/}
         <IntroCard>
           <TextChanger>
             <EndText>Welcome to my space!</EndText>
@@ -121,6 +151,8 @@ const HomePage = () => {
             </p>
           </div>
         </IntroCard>
+
+        {/*Second Card - Small description page*/}
         <AboutCard>
           <h1 className="text-[4rem]">Creative Coder</h1>
           <span className="text-start block ml-14 text-[1.3rem]">
@@ -137,7 +169,9 @@ const HomePage = () => {
             </p>
           </span>
         </AboutCard>
-        <IntroCard>
+
+        {/*Third Card - undefined still*/}
+        <ExperienceCard>
           <h1 className="">
             Hola,
             <br /> Welcome to my space!
@@ -145,16 +179,39 @@ const HomePage = () => {
           <span className="text-end">
             <p>2005 Ecuadorian</p>
           </span>
-        </IntroCard>
-        <IntroCard>
-          <h1 className="">
-            Hola,
-            <br /> Welcome to my space!
-          </h1>
-          <span className="text-end">
-            <p>2005 Ecuadorian</p>
-          </span>
-        </IntroCard>
+        </ExperienceCard>
+
+        {/*Four Card - Access to Blog upcoming*/}
+        <BlogUpComing>
+          <div className="flex flex-col w-[60rem] border-2 p-[1rem]">
+            <div>
+              <h1 className="text-[2rem]">
+                Working on a Blog Project <span>SOON</span>
+              </h1>
+              <p>I will be updating new ideas that I am learning and using.</p>
+            </div>
+            <div></div>
+          </div>
+        </BlogUpComing>
+
+        {/*Five Card - Footer / Get in touch page*/}
+        <FooterCard>
+          <div className="border-2">
+            <h1>Thank you for Checking out me Website!</h1>
+            <h2>Lets get in touch - my Email</h2>
+            <h3>Kevenjq07@gmail.com</h3>
+          </div>
+          <div className="border-2">
+            {/*
+          Here I could maybe add a animation of some sort
+          - plane
+          - train
+          - - depending on theme maybe too
+
+          */}
+            <h1>upcoming animation..</h1>
+          </div>
+        </FooterCard>
       </PageCards>
     </PageContainer>
   );
